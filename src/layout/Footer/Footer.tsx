9fa1199d38f748a810581e8410b12ba0e6e4ea6d
Vasily Ilyuhin <v.ilyuhin@gmail.com>
2022-07-19 10:05:50 +0300

    refactor: fix url problems

--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -1,24 +1,28 @@
 const Footer: React.FC = () => {
-  return(
+  return (
     <footer className="footer">
-    <div className="container">
-      <div className="footer__wrapper">
-        <div className="footer__logo">
-          <a href="https://rs.school/js/" target="_blank" rel="noreferrer">
-            <img src="/icons/rsschool.svg" alt="rsschool" />
-          </a>
-        </div>
-        <div className="footer__info">
-          © 2022 <span>Shoppie</span> All rights reserved
-        </div>
-        <div className="footer__repo">
-          <a href="https://github.com/irrbis38/shoppie" target="_blank"  rel="noreferrer">
-          <img src="/icons/github.svg" alt="repo" />
-          </a>
+      <div className="container">
+        <div className="footer__wrapper">
+          <div className="footer__logo">
+            <a href="https://rs.school/js/" target="_blank" rel="noreferrer">
+              <img src="/icons/rsschool.svg" alt="rsschool" />
+            </a>
+          </div>
+          <div className="footer__info">
+            © 2022 <span>Shoppie</span> All rights reserved
+          </div>
+          <div className="footer__repo">
+            <a
+              href="https://github.com/irrbis38/shoppie"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src="/icons/github.svg" alt="repo" />
+            </a>
+          </div>
         </div>
       </div>
-    </div>
-  </footer>
+    </footer>
   );
 };
 
